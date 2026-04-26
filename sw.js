799588217925adefda97e2b58e43eced58884c87
ACasinha <andrecasinha@gmail.com>
2026-04-26 20:14:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 // Registo Diário de Nacionalidades — Município de Reguengos de Monsaraz
 // ============================================================
 
-const CACHE_NAME   = 'rmz-nacionalidades-v2.3.7.3';
-const CACHE_STATIC = 'rmz-static-v2.3.7.3';
+const CACHE_NAME   = 'rmz-nacionalidades-v3.0.0.1';
+const CACHE_STATIC = 'rmz-static-v3.0.0.1';
 
 const STATIC_ASSETS = [
   './index.html',
