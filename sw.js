971f9baa5005db708b04c9d90107ac7694ac62b3
ACasinha <andrecasinha@gmail.com>
2026-05-30 23:44:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@
 // O pwa.js lê esta constante para mostrar a versão no rodapé.
 // ============================================================
 
-const CACHE_NAME   = 'rmz-nacionalidades-v1.4.7.i';
-const CACHE_STATIC = 'rmz-static-v1.4.7.i';
+const CACHE_NAME   = 'rmz-nacionalidades-v1.4.7.j';
+const CACHE_STATIC = 'rmz-static-v1.4.7.j';
 
 // Todos os assets necessários para a app funcionar offline.
 // Adicionar aqui qualquer novo ficheiro que seja criado.
