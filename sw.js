36127a6c3c81d54f9864ea2e55bbb5fb5b82cfcb
ACasinha <andrecasinha@gmail.com>
2026-06-03 11:25:38 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@
 // O sw-update.js lê esta constante para mostrar a versão no rodapé.
 // ============================================================
 
-const CACHE_NAME   = 'rmz-nacionalidades-v1.5.2.c';
-const CACHE_STATIC = 'rmz-static-v1.5.1.c';
+const CACHE_NAME   = 'rmz-nacionalidades-v1.5.2.d';
+const CACHE_STATIC = 'rmz-static-v1.5.2.d';
 
 const STATIC_ASSETS = [
   './index.html',
