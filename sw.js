02447bf760be920455847b05c90c6d7ffad1067e
ACasinha <andrecasinha@gmail.com>
2026-06-04 17:46:56 +0100

    Update cache version to v1.5.2.k

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@
 // O sw-update.js lê esta constante para mostrar a versão no rodapé.
 // ============================================================
 
-const CACHE_NAME   = 'rmz-nacionalidades-v1.5.2.j';
-const CACHE_STATIC = 'rmz-static-v1.5.2.j';
+const CACHE_NAME   = 'rmz-nacionalidades-v1.5.2.k';
+const CACHE_STATIC = 'rmz-static-v1.5.2.k';
 
 const STATIC_ASSETS = [
   './index.html',
